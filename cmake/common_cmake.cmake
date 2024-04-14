d884281027abf3404afe3da532b3b0935d1e3ad5
Li Bobo <libobo@dobot.cc>
2024-04-14 11:18:15 +0800

    build: delete cmake GITHUB_URL_PREFIX

--- cmake/common_cmake.cmake
+++ cmake/common_cmake.cmake
@@ -1,32 +1,8 @@
 
-if(CMAKE_VERSION VERSION_LESS 3.21)
-    get_directory_property(hasParent PARENT_DIRECTORY)
-    if(NOT hasParent)
-        set(PROJECT_IS_TOP_LEVEL true)
-    else()
-        set(PROJECT_IS_TOP_LEVEL)
-    endif()
-endif()
-if(PROJECT_IS_TOP_LEVEL)
-    option(ENABLE_URL_PREFIX "enable url prefix" OFF)
-    if(ENABLE_URL_PREFIX)
-        if(NOT GITHUB_URL_PREFIX)
-            set(GITHUB_URL_PREFIX https://gh.api.99988866.xyz/) # https://gh.api.99988866.xyz  https://ghproxy.com/ https://toolwa.com/github/
-        endif()
-    else()
-        set(GITHUB_URL_PREFIX "")
-    endif()
-    message("ENABLE_URL_PREFIX: ${ENABLE_URL_PREFIX}, GITHUB_URL_PREFIX: ${GITHUB_URL_PREFIX}")
-
-    # set(FETCHCONTENT_BASE_DIR ${CMAKE_SOURCE_DIR}/.third_party/)
-    # set(FETCHCONTENT_SOURCE_DIR_COMMON_CMAKE ${CMAKE_SOURCE_DIR}/.third_party/common_cmake-src)
-    set(FETCHCONTENT_QUIET OFF)
-endif()
-
 include(FetchContent)
 FetchContent_Declare(
     common_cmake
-    GIT_REPOSITORY ${GITHUB_URL_PREFIX}https://github.com/xiaohaidao/common_cmake.git
+    GIT_REPOSITORY https://github.com/xiaohaidao/common_cmake.git
     GIT_TAG dev
     GIT_SHALLOW ON
     GIT_PROGRESS ON
